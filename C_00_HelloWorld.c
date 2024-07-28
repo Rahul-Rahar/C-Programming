e5f7a05f3e2bccbf9e934e3d9ffca2c92f26ca78
RAHUL RAHAR <140370820+Rahul-Rahar@users.noreply.github.com>
2024-07-28 14:28:50 +0530

    Update C_00_HelloWorld.c

--- C_00_HelloWorld.c
+++ C_00_HelloWorld.c
@@ -1,6 +1,7 @@
-#include<stdio.h>
+#include<stdio.h> // This is a 'Header file library' that lets us work with input and output.
 
-int main(){
-    printf("Hello World!");
-    return 0;
+int main() // Main() is function. Any code between its curly brackets {} will be executed.
+{
+    printf("Hello World!"); // printf() is a function to print or give output on the user or computer screen
+    return 0; // return 0 stops the code execution inside the main(){} function.
 }
