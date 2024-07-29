0fcefa2ec5632fdba9535164594c3ee5e4d913d9
RAHUL RAHAR <140370820+Rahul-Rahar@users.noreply.github.com>
2024-07-29 14:17:55 +0530

    Basic Two Numbers Adding Program without Understanding.

---
+++ C_00_Sample_Program.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+
+// Writing a Sample Program of Adding two Numbers in C language without Understanding.
+int main() {
+
+    int a, b;
+        printf("Enter Number a\n");
+        scanf("%d", &a);
+
+
+        printf("Enter Number b\n");
+        scanf("%d", &b);
+
+        printf("The Sum is %d\n", a+b);
+
+        return 0;
+
+
+}
