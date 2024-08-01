fbf9def9a87d45362e0a15f87ffa1698edff2db8
RAHUL RAHAR <140370820+Rahul-Rahar@users.noreply.github.com>
2024-08-01 22:38:51 +0530

    Variables in C

---
+++ Ch01_01_Variables.c
@@ -0,0 +1,37 @@
+#include<stdio.h>
+
+
+        // VAERIABLES :- Variables are names given to a Memory Location
+        // Variables Decared and initialized by writing type variable_name = value;
+int main() {
+    int a = 5; // Here 'a' is variable in which '5' is Value assigned to it. Thus 'a' is a memory location where '5' is reserved.
+    char _rahul = 'B'; // Here _rahul is also a Variable where 'B' assigned to it.
+    
+
+    // Some rules for naming a variable:
+    // 1. Variable name can contain Alphabets, Underscore(_), Digits
+    // 2. Can Start only with Alphabets or Underscore(_) only and can't start with a Digit, Special Characters.
+
+    // 3. No whitespace and reserved keywords are allowed. Like (rahul rahar) here between space not allowed.
+
+    // 4. Variable name should be unique.
+    
+    // 5. Variable name should not be too long.
+    
+    // 6. Variable name should be meaningful.
+    
+    // 7. Variable name should be easy to understand.
+
+    // Valid Variables Name:
+    int hello;
+    float erics123;
+    char _nice34;
+
+    // Invalid Variables :
+    /* int 123hello; // Here '123' is a Digit and can't be at
+       
+       int hello world; // Here ' ' is a whitespace and can't be in variable name
+    */
+
+   return 0;
+}
