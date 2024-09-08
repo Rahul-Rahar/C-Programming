825aa2fb2f44ff308f0c321b3b47d16ef2ad0df7
RAHUL RAHAR <140370820+Rahul-Rahar@users.noreply.github.com>
2024-09-08 19:26:59 +0530

    Simple Multiplication Table Using Operators only

---
+++ Ch02_07_Multiplication_Table.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+int main () {
+
+ // Simple Multiplication Table using only Operators in C
+
+  
+  printf("Enter the Number you want Multiplication Table of: ");
+  int a;
+  scanf(" %d", &a);
+  printf("\nThe Multiplication Table of Entered Number %d is: ", a);
+
+  printf("\n%d x 1: %d\n", a, a*1);
+  printf("%d x 2: %d\n", a, a*2);
+  printf("%d x 3: %d\n", a, a*3);
+  printf("%d x 4: %d\n", a, a*4);
+  printf("%d x 5: %d\n", a, a*5);
+  printf("%d x 6: %d\n", a, a*6);
+  printf("%d x 7: %d\n", a, a*7);
+  printf("%d x 8: %d\n", a, a*8);
+  printf("%d x 9: %d\n", a, a*9);
+  printf("%d x 10: %d\n", a, a*10);
+  
+  
+}
