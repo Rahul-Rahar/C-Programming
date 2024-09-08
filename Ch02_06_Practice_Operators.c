8f685dbd6152f30ba8a46ef601b194822728ce77
RAHUL RAHAR <rahulrahar02800@gmail.com>
2024-09-08 13:38:40 +0000

    modified:   Ch02_06_Practice_Operators.c

--- Ch02_06_Practice_Operators.c
+++ Ch02_06_Practice_Operators.c
@@ -2,6 +2,37 @@
 
 int main() {
 
+       /* 1.
+        Write a C program that performs the following operations:
+
+1. Declare two integer variables a and b, and assign them values.
+
+2. Use arithmetic operators (+, -, *, /, %) to calculate and print:
+
+a). The sum of a and b
+b). The difference when b is subtracted from a
+c). The product of a and b
+d). The quotient when a is divided by b
+e). The remainder when a is divided by b
+f). Declare a float variable c and assign it a value.
+
+3. Use the assignment operators (+=, -=, *=, /=, %=) to perform the following:
+
+a). Add 5 to c
+b). Subtract 3 from c
+c). Multiply c by 2
+d). Divide c by 4
+e). Use the increment (++) and decrement (--) operators on a and b, and print their updated values.
+
+4. Use relational operators (==, !=, >, <, >=, <=) to compare a and b and print the results of the comparisons:
+
+a). Is a equal to b? 
+b). Is a not equal to b? 
+c). Is a greater than b? 
+d). Is a less than or equal to b? 
+       
+       */ 
+
   int a = 34;
   int b = 32;
   printf("Values of a and b are: %d\n%d\n", a, b);
@@ -34,6 +65,28 @@ int main() {
   printf("Is a equal to b?: %d\n Is a not equal to b?: %d\nIs a greater than "
          "b?: %d\nIs a less than or equal to b?: %d\n",
          a == b, a != b, a > b, a <= b);
+
+
+/* Exercise 2.
+       1. Declare three integer variables x, y, and z, and assign them values.
+
+2. Use logical operators to perform the following:
+
+a). Check if both x and y are greater than zero.
+b). Check if either x or y is less than zero.
+c). Use the NOT operator to check if z is not equal to zero.
+d). Combine relational and logical operators to check if x is greater than y and y is greater than z.
+
+3. Use relational operators to compare x, y, and z, and use logical operators to:
+
+a). Check if all three numbers are positive.
+b). Check if at least one of the three numbers is negative.
+
+4. Add expressions using both arithmetic and logical operators to evaluate complex conditions, such as:
+
+a). (x + y) > z && x > y
+b). (x * y) <= z || z != 0
+*/
 
   // Logical Operators
   int x, y, z;
